e861315ce0c393c733b620657c6929fcde242108
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-02-16 14:53:46 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(void) 
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 1
+ */
+int main(void)
 {
-    fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return 1;
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
 }
