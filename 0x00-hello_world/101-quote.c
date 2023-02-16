9318e9152770ed891e31447cd68cb1cc3b0d4fe0
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-02-16 15:10:41 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
 	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	      s);
 	return (1);
 }
