cbe1b0868729c60e742c8a38fbd4adabb2e6d9a9
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-02-16 14:56:08 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdlib.h>
+
 /**
- * main - main block
- * Return: 1
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
