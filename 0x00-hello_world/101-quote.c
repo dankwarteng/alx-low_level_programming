9e98c2214ff0d7e1e0344dac6b48601a4441002c
dankwarteng <dankwarteng@yahoo.com>
2023-02-16 06:46:04 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 /**
- * main -A c program that prints  a line to the standard error
- * Return: 1 (Success)
+ * main - main block
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
