ed2d6d0decedb04d59b9224b83b5d9655ce217bf
dankwarteng <dankwarteng@yahoo.com>
2023-02-16 06:15:36 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
- * main - main function in the task
- *
- *
- *Return: return 1
-*/
+ * main - main block
+ * Return: 1
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
+}
