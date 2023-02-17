0c7d8c5904050bcbb7b09d76dd3ba3809839ed96
dankwarteng <dankwarteng@yahoo.com>
2023-02-17 05:58:00 -0800

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: Print numbers from 0 to 99.
- * Numbers must be separated by commas followed by a space.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 5 times.
- * You are not allowed to use variables of type `char`.
- * Return: 0
- */
+ *main - Prints all combinations of two digits with,
+ * and space followed by new line
+ *
+ *Return: returns 0
+*/
 int main(void)
 {
-	int i, j, k;
-
-	for (i = 0; i < 100; i++)
-	{
-		j = i / 10;
-		k = i % 10;
-
-		putchar(j + '0');
-		putchar(k + '0');
-
-		if (i < 99)
-		{
-			putchar(44);
-			putchar(32);
-		}
-	}
-	putchar('\n');
-
-	return (0);
+int digit1, digit2;
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+if (digit1 == 8 && digit2 == 9)
+continue;
+putchar(',');
+putchar(' ');
 }
+}
+putchar('\n');
+return (0);
+}
+
