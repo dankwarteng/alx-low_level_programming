00c08c7b460d6479705fc327ea6b71a0c209f4c9
dankwarteng <dankwarteng@yahoo.com>
2023-02-21 04:23:30 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_alphabet - function that print alphabet
+ *it prints in lowercase
+ *Return: no return
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
