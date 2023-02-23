3edb68b0f0fba161a458feafeb7996fdac6cee4c
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-02-23 14:15:14 +0000

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 /**
- *  * _isupper - check if c is upper
- *   *
- *    * @c: input for alphabet
- *     *
- *      * Return: 1 if its uppercase, 0 if not
- *      */
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
 
 int _isupper(int c)
 {
-		if (c >= 65 && c <= 90)
-					return (1);
-			else
-						return (0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
