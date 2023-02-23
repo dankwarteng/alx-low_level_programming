11bc919a1f3fe72ac133f33622789401f3967fb8
dankwarteng <dankwarteng@yahoo.com>
2023-02-23 06:26:03 -0800

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,8 @@
  *
  * @c: input for alphabet
  *
-<<<<<<< HEAD
- * Return: 1 if its uppercase, 0 if no
- */
-=======
  * Return: 1 if its uppercase, 0 if not
-*/
->>>>>>> 3edb68b0f0fba161a458feafeb7996fdac6cee4c
+ */
 
 int _isupper(int c)
 {
