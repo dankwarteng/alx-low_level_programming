7e69a6b98edfdff5d0bcb5bd9a962bcafa37eb5b
dankwarteng <dankwarteng@yahoo.com>
2023-03-23 07:04:10 -0700

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,41 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdarg.h>
+
+
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct p - checker struct
+ * @p: possibility
+ * @f: Function to be used
+ */
+typedef struct p
+{
+  char *p;
+  void (*f)(va_list);
+} checker;
+
+void print_c(va_list);
+void print_i(va_list);
+void print_f(va_list);
+void print_s(va_list);
+
+#endif
