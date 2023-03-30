3f253f26353c0d556a2f696b65849897a4f1584c
dankwarteng <dankwarteng@yahoo.com>
2023-03-30 12:12:07 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_list - Frees a list
+ *@head: Pointer to the list head
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
