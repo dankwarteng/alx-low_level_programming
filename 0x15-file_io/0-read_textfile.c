e70f4d22f857ea9dae28911ee37d6a085a91ab65
dankwarteng <dankwarteng@yahoo.com>
2023-04-08 11:31:44 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_d;
+	int b_rd, b_w;
+	char *buffer;
+	if (filename == NULL)
+		return (0);
+	file_d = open(filename, O_RDONLY);
+	if (file_d < 0)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	b_rd = read(file_d, buffer, letters);
+	if (b_rd < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[b_rd] = '\0';
+	close(file_d);
+	b_w = write(STDOUT_FILENO, buffer, b_rd);
+	if (b_w < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (b_w);
+}
