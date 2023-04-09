a28e8f738006972196f965030c171220aa67b7b3
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 08:53:47 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ file_d = open(filename, O_RDONLY);
 
 b_w = write(STDOUT_FILENO, buffer, read(file_d, buffer, letters));
 
-if (file_d == -1) || if (b_w == -1)
+if (file_d == -1 || b_w == -1)
 return (0);
 	
 close(file_d);
