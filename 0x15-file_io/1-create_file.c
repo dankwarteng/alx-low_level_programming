ae23ca0acf4d29d79af3273476ad831114ff3670
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 11:08:21 +0000

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,29 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_d, w_file;
+	int fdes, wrtfile;
 
 	if (filename == NULL)
 		return (-1);
-	file_d = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	if (file_d == -1) 
+	fdes = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fdes == -1) 
 	{
 		return (-1);
 	}
 
 	if (text_content == NULL)
 		return (1);
-	w_file= write(file_d, text_content, strlen(text_content));
-	close(file_d);
-	if (w_file == -1)
+
+	wrtfile = write(fdes, text_content, strlen(text_content));
+	close(fdes);
+	if (wrtfile == -1) 
 	{
-		close(file_d); 
+		close(fdes); 
 		return (-1);
 	}
 
 	return (1);
+
 }
