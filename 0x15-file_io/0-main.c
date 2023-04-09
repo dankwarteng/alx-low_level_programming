6fef40d62a9ce3437e10baa9e90f7fdb7618a392
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 07:49:43 +0000

    Update 0-main.c

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -11,10 +11,10 @@ int main(int ac, char **av)
 {
 ssize_t n;
 
-if (ac != 2)
+if (ac  !=  2)
 {
-  dprintf(2, "Usage: %s filename\n", av[0]);
-  exit(1);
+dprintf(2, "Usage: %s filename\n", av[0]);
+exit(1);
 }
 n = read_textfile(av[1], 114);
 printf("\n(printed chars: %li)\n", n);
