2c20584473f793778fc516dc97d5824f9df331a4
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 06:54:49 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
  * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
  * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
