f642b15ea7ce77b739f605e41cda3d425ec514d0
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 09:42:20 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #define BUFFERSIZE 1204
 /**
-  * main - Copies content.
+  * 3-cp - copies the content of a file to another file.
   * @argc: Arguments count.
   * @argv: Files names.
   * Return: 0 on success
