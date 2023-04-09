1849733cc7661c8959ddba87fccd0c14bbb07c37
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 10:35:05 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,24 +7,24 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file_d,b_w;
+int file_d,size;
 char *buffer;
 buffer = malloc(sizeof(char) * letters);
 
-/*Check content file*/	
-if (!filename)
+	
+if (filename == NULL)
 return (0);
-/*Check value of buffer*/
-if (!buffer)
+
+if (buffer == NULL)
 return (0);
-/*Open file*/
+
 file_d = open(filename, O_RDONLY);
-/*Read file*/
+
 b_w = write(STDOUT_FILENO, buffer, read(file_d, buffer, letters));
-if (file_d == -1 || b_w == -1)
+if (file_d == -1 || size == -1)
 return (0);	
 close(file_d);
 free(buffer);
-return (b_w);
+return (size);
 }
 
