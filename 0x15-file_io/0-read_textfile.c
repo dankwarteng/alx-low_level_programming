b86dced65bbe49669617c5484822d9cf425d4a3f
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 08:30:49 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,32 @@
 #include "main.h"
-
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: file to read
  * @letters: the number of letters it should read and print
  * Return: the actual number of letters it could read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_d,b_rd,b_w;
-	char *buffer;
-                buffer = malloc(sizeof(char) * letters);
-                b_rd = read(file_d, buffer, letters);
+int file_d,b_rd,b_w;
+char *buffer;
+buffer = malloc(sizeof(char) * letters);
+b_rd = read(file_d, buffer, letters);
 	
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-                 if (buffer == NULL)
-		return (0);
+if (buffer == NULL)
+return (0);
 
-	file_d = open(filename, O_RDONLY);
+file_d = open(filename, O_RDONLY);
 
-                b_w = write(STDOUT_FILENO, buffer, b_rd);
+b_w = write(STDOUT_FILENO, buffer, b_rd);
 
-	if (file_d < 0)
-		return (0);
-
-	if (b_rd < 0)
-		return (0);
+if (file_d < 0) || if (b_rd < 0)
+return (0);
 	
-	close(file_d);
-	free(buffer);
-	return (b_w);
+close(file_d);
+free(buffer);
+return (b_w);
 }
-
 
