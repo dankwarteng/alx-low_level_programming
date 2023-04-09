dd1b4789801a1b8250bdf7f85f25c3aadd523e71
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-09 08:04:55 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,29 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
- */
-
+* read_textfile - Reads a text file and prints in POSIX stdout.
+* @filename: File 2 read.
+* @letters: Number of letter it should read and print.
+* Return: Actual number of letter it could read and print.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_d;
-	int b_rd, b_w;
-	char *buffer;
-	
-	if (filename == NULL)
-		return (0);
-	file_d = open(filename, O_RDONLY);
-	if (file_d < 0)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-	b_rd = read(file_d, buffer, letters);
-	if (b_rd < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	buffer[b_rd] = '\0';
-	close(file_d);
-	b_w = write(STDOUT_FILENO, buffer, b_rd);
-	if (b_w < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	free(buffer);
-	return (b_w);
+int fdest, size;
+char *buffer;
+buffer = malloc(sizeof(char) * letters);
+
+/*Check content file*/
+if (!filename)
+return (0);
+/*Check value of buffer*/
+if (!buffer)
+return (0);
+/*Open file*/
+fdest = open(filename, O_RDONLY);
+/*Read file*/
+size = write(STDOUT_FILENO, buffer, read(fdest, buffer, letters));
+if (fdest == -1 || size == -1)
+return (0);
+close(fdest);
+free(buffer);
+return (size);
 }
