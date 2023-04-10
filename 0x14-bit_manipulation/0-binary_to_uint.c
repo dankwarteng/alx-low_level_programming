3647256e569bd47af0299a0e6cfeae3013693e3b
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-10 23:36:49 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,16 +16,16 @@ unsigned int binary_to_uint(const char *b)
 
 	int x = 0;
 
-	while (b[x] != '\0')
-		if (b[x] != '0'  &&  b[x] != '1')
-	 x++;
+	for (x = 0; b[x] != '\0'; x++)
+		if (b[x]  != '0' && b[x] != '1')
 			return (0);
-	while (b[x] != '\0')
+			
+	for (x = 0; b[x] != '\0'; x++)
 	{
 	val *= 2;
 	if (b[x] == '1')
 	val += 1;
-	x++;
+	
 	}
 	return (val);
 }
