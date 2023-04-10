fde7fc7b117e92db50a1cc57f518d328d00d8999
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-10 23:46:57 +0000

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n &= ~(1 << index);
+	number &= ~(1 << index);
+	*n = number;
 
 	return (1);
 }
