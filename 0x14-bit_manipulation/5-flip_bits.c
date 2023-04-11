a3cc241def1f0fd3a7e98827f8ee1716343e35fd
Dennis Nana Acheampong <122838980+dankwarteng@users.noreply.github.com>
2023-04-11 00:18:15 +0000

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
         
 		
 	number = n ^ m;
-	for (i = n ^ m; i < 10; i >>= 1)
+	for (i = n ^ m; i > 0; i >>= 1)
 	{
 		if (i & 1)
 		count++;
